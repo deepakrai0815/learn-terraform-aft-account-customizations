c7f6d525db43f62a6c0e7776cfe7a18dbf5ade89
unknown <Administrator@ESSNOIDEVL002.ess.com>
2026-04-26 21:48:35 +0530

    revert

--- sandbox/terraform/ssm.tf
+++
@@ -1,6 +0,0 @@
-resource "aws_ssm_parameter" "test_param" {
-  name      = "/aft/test/account-id"
-  type      = "String"
-  value     = data.aws_caller_identity.current.account_id
-  overwrite = true
-}
